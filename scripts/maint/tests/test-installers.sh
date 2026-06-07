2a0efe6be7de17323fee81d927abdf2475016e23
sairuk <sairuk@users.noreply.github.com>
2026-06-07 07:23:54 +0000

    Rollup testing to main (#489)
    * add a default colour scheme for #481 [sairuk]
    * update desktop mode [sairuk]
    * tweak desktop and novnc access [sairuk]
    * remove unneeded netmount firewall dep [sairuk]
    * add rando mister cores, fix samba firewalling [sairuk]
    * updates tests [sairuk]
    * update cockpit firewall rules [sairuk]
    * prefix all our firewalld files with retronas- [sairuk]
    * fix netatalk4 compile [sairuk]
    * add installer tests, fix romimport [sairuk]
    * correct romm directory creation [sairuk]
    * fix path creation, bunch more clean up, repair netmount install [sairuk]
    * add netn00t install (#484) [BluejayWagon]
    * fix display name [sairuk]
    * Add missing ops2l to e128 entry (#483) [derek-oneill]
    * fix role, and service name [sairuk]
    * update assembly64 installer, use desktop mode [sairuk]
    * review and cleanup [sairuk]
    * move all firewall based installers to firewalld role [sairuk]
    * only install our firewall rules if our retro zone has been installed [sairuk]
    * add romvault gui for desktop mister-organize [sairuk]
    * update adtpro [sairuk]
    * update desktop role [sairuk]
    * move adtpro to use desktop mode [sairuk]
    * add fluxbox based desktop role [sairuk]
    * make sure fsp can set requested perms on run [sairuk]
    * update mystic bbs filearea generator to work with new systems_map [sairuk]
    * update to romvault 3.7.5 [sairuk]
    * more speed fuzz, deprecate extradirs role [sairuk]
    * commit current optimisation work [sairuk]

---
+++ scripts/maint/tests/test-installers.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+#set -e
+
+MODE="all"
+PLAYBOOK="install_*.yml"
+
+while getopts "m:p:" OPT
+do
+  case ${OPT} in
+    m)
+      MODE=${OPTARG}
+      ;;
+    p)
+      PLAYBOOK=$(basename ${OPTARG})
+      ;;
+    *)
+      usage
+      ;;
+  esac
+done
+
+OUTPATH=/tmp/rtrn_inst_test
+KNOWN=/opt/retronas/scripts/maint/tests/test-installers.known
+
+cd /opt/retronas/ansible
+[ ! -d ${OUTPATH} ] && mkdir -p $OUTPATH
+
+usage() {
+  echo " -m mode [report-only, all]"
+  echo " -p playbook [install_playbook.yml]"
+  exit 1
+}
+
+fail_report(){
+  local FAILMSG="${1:-fatal}"
+  for FILE in `ls $OUTPATH/*.log`
+  do
+    BASENAME=$(basename $FILE .yml.log)
+    FAILURE=$(grep "${FAILMSG}" $FILE)
+    if [ ! -z "$FAILURE" ]
+    then
+      REGEX=$(grep $BASENAME $KNOWN | cut -d',' -f2-)
+      echo "|-> [${BASENAME}] ${FAILURE}" | grep -E "$REGEX"
+    fi
+  done
+
+}
+
+if [ $MODE == "all" ]
+then
+  rm -f $OUTPATH/*.log &> /dev/null
+  echo "TESTING Started $(date +'%Y-%m-%d %H:%M:%S')"
+  for PLAY in `find -maxdepth 1 -type f -iname "${PLAYBOOK}"`
+  do
+    echo $PLAY
+    ansible-playbook -Cvv "${PLAY}" &> "${OUTPATH}/${PLAY}.log"
+    #[ $? -ne 0 ] && echo "Failed on ${PLAY}"
+  done
+  echo "TESTING Ended $(date +'%Y-%m-%d %H:%M:%S')"
+fi
+
+echo -e "\n\n"
+echo "REPORT"
+echo "- fatal ---------------------------------------------------------------------"
+echo "|"
+fail_report fatal
+echo "-----------------------------------------------------------------------------"
+
+
+echo "- task failed ---------------------------------------------------------------"
+echo "|"
+fail_report "Task failed"
+echo "-----------------------------------------------------------------------------"
+echo REPORT END
